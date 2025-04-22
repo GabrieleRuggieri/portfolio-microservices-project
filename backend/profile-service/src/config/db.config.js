fee745671b269e0dab3e7b7991072ca61f9ec9e5
GabrieleRuggieri <gabri20022002@gmail.com>
2025-04-22 09:31:53 +0200

    new db config

--- backend/profile-service/src/config/db.config.js
+++ backend/profile-service/src/config/db.config.js
@@ -7,8 +7,8 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'portfolio_profile',
+    password: process.env.DB_PASSWORD || 'admin',
+    database: process.env.DB_NAME || 'portfolio_services',
     dialect: 'mysql',
     pool: {
         max: 5,
